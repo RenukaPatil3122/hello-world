f22b577ed4e7241873a6a439ce912fa6de2c2975
Renuka Patil <renukapatil627@gmail.com>
2026-03-14 16:20:16 +0530

    state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Message />
         {/* <Greet />
         <Welcome /> */}
         {/* <Hello /> */}
-        <Greet name="Bruce" heroName="Batman">
+        {/* <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
@@ -21,7 +23,7 @@ class App extends Component {
         <Greet name="Diana" heroName="Wonder Woman" />
         <Welcome name="Bruce" heroName="Batman" />
         <Welcome name="Clark" heroName="Superman" />
-        <Welcome name="Diana" heroName="Wonder Woman" />
+        <Welcome name="Diana" heroName="Wonder Woman" /> */}
       </div>
     );
   }
