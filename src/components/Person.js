6d962416a16e6d1aae1a5e151e0edc886e1e5d5c
Renuka Patil <renukapatil627@gmail.com>
2026-03-15 22:51:37 +0530

    not accessible in child comp

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-function Person({ person, key }) {
+function Person({ person }) {
   return (
     <div>
       <h2>
-        {key}I am {person.name}. I am {person.age} years old. I know{" "}
-        {person.skill}
+        I am {person.name}. I am {person.age} years old. I know {person.skill}
       </h2>
     </div>
   );
