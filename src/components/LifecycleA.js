9b5edb3b23f81e891990d9d2a99fe68df0538e38
Renuka Patil <renukapatil627@gmail.com>
2026-03-17 16:18:39 +0530

    Updating lifecycle

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -20,11 +20,32 @@ class LifeCycleA extends Component {
     console.log("LifeCycleA componentDidMount");
   }
 
+  shouldComponentUpdate() {
+    console.log("LifeCycleA shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("LifeCycleA getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifeCycleA componentDidUpdate");
+  }
+
+  changeState = () => {
+    this.setState({
+      name: "Renuka",
+    });
+  };
+
   render() {
     console.log("LifeCycleA render");
     return (
       <div>
         <div>LifeCycle A</div>
+        <button onClick={this.changeState}>Change State</button>
         <LifecycleB />
       </div>
     );
--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -19,6 +19,19 @@ class LifeCycleB extends Component {
     console.log("LifeCycleB componentDidMount");
   }
 
+  shouldComponentUpdate() {
+    console.log("LifeCycleB shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("LifeCycleB getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifeCycleB componentDidUpdate");
+  }
   render() {
     console.log("LifeCycleB render");
     return <div>LifeCycle B</div>;
