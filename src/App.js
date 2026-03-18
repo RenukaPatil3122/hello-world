8e6c3bcf325d37cc718756886f2074fed8b143e5
Renuka Patil <renukapatil627@gmail.com>
2026-03-18 09:59:50 +0530

    Refs

--- src/App.js
+++ src/App.js
@@ -21,12 +21,15 @@ import LifecycleA from "./components/LifecycleA";
 import FragmentDemo from "./components/FragmentDemo";
 import Table from "./components/Table";
 import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ParentComp />
+        <RefsDemo />
+
+        {/* <ParentComp /> */}
 
         {/* <Table /> */}
 
