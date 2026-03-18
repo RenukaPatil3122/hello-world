d3c093e856062429a88cef6fd11bfdba03eef696
Renuka Patil <renukapatil627@gmail.com>
2026-03-18 10:46:07 +0530

    Refs with Class components

--- src/App.js
+++ src/App.js
@@ -22,12 +22,15 @@ import FragmentDemo from "./components/FragmentDemo";
 import Table from "./components/Table";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RefsDemo />
+        <FocusInput />
+
+        {/* <RefsDemo /> */}
 
         {/* <ParentComp /> */}
 
