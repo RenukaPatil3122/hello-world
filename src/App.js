d1f834434346129da95f0729750cbe193c563890
Renuka Patil <renukapatil627@gmail.com>
2026-03-18 12:12:35 +0530

    Portals

--- src/App.js
+++ src/App.js
@@ -24,12 +24,15 @@ import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FRParentInput />
+        <PortalDemo />
+
+        {/* <FRParentInput /> */}
 
         {/* <FocusInput /> */}
 
